981d0bc9f0ebc2c2a7b6caac63e62b9d0dd39bf9
Joshua Coady <josh@lovetoknow.com>
2023-08-30 07:52:53 -0700

    Revert "Convert various line breaks to the Slack's ones"

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ const core = require('@actions/core');
 const slackifyMarkdown = require('slackify-markdown');
 
 try {
-    const input = core.getInput('text', { required: true });
-    const mrkdwn = slackifyMarkdown(input);
-    const output = mrkdwn.replace(/\r\n|\r|\n/g, "\\n");
-    core.setOutput("text", output);
+    const md = core.getInput('text', {required: true});
+    const mrkdwn = slackifyMarkdown(md);
+    core.setOutput("text", mrkdwn);
 } catch (error) {
     core.setFailed(error.message);
 }
