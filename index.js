765edd22e362a9d82630030d2dadd9661edad99c
Hasier <hasier@gr4vy.com>
2024-04-16 15:18:52 +0100

    Add input variable to stringify result

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const slackifyMarkdown = require('slackify-markdown');
 
 try {
     const input = core.getInput('text', { required: true });
+    const jsonify = core.getBooleanInput('jsonify', { required: false });
     const mrkdwn = slackifyMarkdown(input);
-    const output = mrkdwn.replace(/\r\n|\r|\n/g, "\\n");
+    // trim start/end " from JSON string for backwards compatibility
+    const output = jsonify ? JSON.stringify(mrkdwn).replace(/^"+|"+$/g, '') : mrkdwn;
     core.setOutput("text", output);
 } catch (error) {
     core.setFailed(error.message);
